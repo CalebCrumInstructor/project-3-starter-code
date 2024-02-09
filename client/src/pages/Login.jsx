1651986532b807d6e9667db251a35b0aa628ecae
kathatbat <katoria1234@icloud.com>
2024-02-08 18:40:11 -0600

    commit login changes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,7 +4,9 @@ import { LOGIN_USER } from "../graphql/mutations";
 import { useSelector } from "react-redux";
 import { getUser } from "../redux/slices/userSlice";
 import { Navigate } from "react-router-dom";
-
+import { Button, Container } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import Background from "../../public/images/treasure map.png";
 import Page from "../components/Page";
 import AuthService from "../utils/auth";
 
@@ -12,16 +14,18 @@ const styles = {
   form: {
     display: "flex",
     flexDirection: "Column",
-    width: "300px",
+    width: "100%",
+    backgroundImage: `url(${Background})`,
   },
-  submitBtn: {
+  Button: {
     cursor: "pointer",
+    backgroundColor: "DodgerBlue",
   },
 };
 
 const headContent = (
   <>
-    <title>Change Me! - Login</title>
+    <title>Login</title>
     <meta name="description" content="Login for Project-3 Starter Code." />
   </>
 );
@@ -63,34 +67,53 @@ export default function Login() {
   }
 
   return (
-    <Page isProtected={false} headContent={headContent}>
-      <div>Login</div>
-      <form style={styles.form} onSubmit={handleFormSubmit}>
-        <input
-          placeholder="Email"
-          name="email"
-          type="email"
-          value={formState.email}
-          onChange={handleChange}
-        />
-        <input
-          placeholder="Password"
-          name="password"
-          type="password"
-          value={formState.password}
-          onChange={handleChange}
-        />
-        {loading ? (
-          <button type="submit" disabled={true} style={styles.submitBtn}>
-            Loading...
-          </button>
-        ) : (
-          <button type="submit" style={styles.submitBtn}>
-            Submit
-          </button>
-        )}
-      </form>
-      {error && <h3>{error.message}</h3>}
-    </Page>
+    // <Page isProtected={false} headContent={headContent}>
+    //   <div>Login</div>
+    //   <form style={styles.form} onSubmit={handleFormSubmit}>
+    //     <input
+    //       placeholder="Email"
+    //       name="email"
+    //       type="email"
+    //       value={formState.email}
+    //       onChange={handleChange}
+    //     />
+    //     <input
+    //       placeholder="Password"
+    //       name="password"
+    //       type="password"
+    //       value={formState.password}
+    //       onChange={handleChange}
+    //     />
+    //     {loading ? (
+    //       <button type="submit" disabled={true} style={styles.submitBtn}>
+    //         Loading...
+    //       </button>
+    //     ) : (
+    //       <button type="submit" style={styles.submitBtn}>
+    //         Submit
+    //       </button>
+    //     )}
+    //   </form>
+    //   {error && <h3>{error.message}</h3>}
+    // </Page>
+    <Container>
+    <Form isprotected="false" headcontent={headContent}> 
+    <Form.Group style={styles.form} className="mb-3" controlId="formBasicEmail" onSubmit={handleFormSubmit}>
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" name="email" value={formState.email} onChange={handleChange}/>
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group style={styles.form} className="mb-3" controlId="formBasicPassword" onSubmit={handleFormSubmit}>
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" name="password" value={formState.password} onChange={handleChange}/>
+    </Form.Group>
+    <Button style={styles.Button} variant="primary" type="submit">
+      Submit
+    </Button>
+  </Form>
+  </Container>
   );
 }
